7cabf0300eafe4da5cb4aeaea2faa8004d6e17c0
Reese Stowell <Reese2as@gmail.com>
2021-09-11 21:08:07 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Reese Stowell
+ */
+package org.example;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args)
+    {
+        Scanner user_input = new Scanner(System.in);
+        String noun, verb, adjective, adverb;
+        System.out.print("Enter a noun ");
+        noun = user_input.next();
+        System.out.print("Enter a verb ");
+        verb = user_input.next();
+        System.out.print("Enter a adjective ");
+        adjective = user_input.next();
+        System.out.print("Enter a adverb ");
+        adverb = user_input.next();
+        System.out.println(String.format("Do you %s her %s %s %s? you're a goofy guy.", verb, adjective, noun, adverb));
+    }
+}
